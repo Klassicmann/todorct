09dc78dfd01ce32c6ec581bccd65990cd798cc3d
unknown <valyoharmless@gmail.com>
2020-04-13 12:45:22 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import uuid from "uuid";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/todoInput";
 import TodoList from "./components/todoList";
 
